5aa7bd582e304424185d90d6787aa8ec37f55428
progEf <programist22869@gmail.com>
2024-07-21 20:53:31 +0300

    beta version

--- Search/views.py
+++ Search/views.py
@@ -26,18 +26,26 @@ class ProductList(generics.ListCreateAPIView):
         # x_filter_value = headers.get('HTTP_CITY')  # Пример чтения значения из заголовка
         if query_params:
             results = City.objects.filter(
-                Q(name__contains=query_params['city']) | Q(subject__contains=query_params['city']))
+                  Q(subject__contains=query_params['city']) | Q(name__contains=query_params['city']))
             return results
         return super().get_queryset()  # Если нет параметров в заголовках, возвращаем исходный queryset
 
 
 class CitySearchWeather(APIView):
     def get(self, request):
-        idCity = request.query_params['IdCity']
-        results = City.objects.filter(id=idCity)
-        serializer = CitySerializer(results, many=True).data[0]
-        weather = RequestWeather().PerDay(lat=serializer['lat'],lon=serializer['lon'])
-        return Response({'name':serializer['name'], 'subject' : serializer['subject'] ,'weather': weather})
+
+        OneCity = request.query_params['OneCity']
+        CityTwo = request.query_params['CityTwo']
+        CityThree = request.query_params['CityThree']
+        Arr = [OneCity, CityTwo, CityThree]
+        ListCity= []
+        for i in Arr:
+            if i != 'null':
+                results = City.objects.filter(id=i)
+                serializer = CitySerializer(results, many=True).data[0]
+                weather = RequestWeather().PerDay(lat=serializer['lat'], lon=serializer['lon'])
+                ListCity.append({'name': serializer['name'], 'subject': serializer['subject'], 'weather': weather})
+        return Response(ListCity)
 
 
 class HomePage(View):
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -8,11 +8,16 @@ $(document).ready(function () {
             $('#Hints').html("");
         }
     });
+    const myDiv = document.getElementById('main-input');
 
-    $('#find-city').on('blur', function () {
-        $('#Hints').html("");
-        // Убираем подсказки при потере фокуса
+// Добавляем обработчик события клика на весь документ
+    document.addEventListener('click', function (event) { // Убираем подсказки при потере фокуса
+        // Проверяем, является ли цель клика дочерним элементом вашего div
+        if (!myDiv.contains(event.target)) {
+            $('#Hints').html("");
+        }
     });
+    var json;
 
     function searchCity(cityName) {
         fetch('/products?city=' + cityName)
@@ -21,19 +26,81 @@ $(document).ready(function () {
                 $('#Hints').html("");
                 $('#Hints').append('<div id="suggestions"></div>')
                 const suggestionsDiv = $('#suggestions');
-                console.log(data.length)
                 if (data.length === 0) {
                     const suggestion = $('<div>').addClass('city-list').text('Нету');
                     suggestionsDiv.append(suggestion);
                 }
                 data.forEach(city => {
-                    const suggestion = $('<div>').addClass('city-list').text(city.name);
+                    const suggestion = $('<div>').addClass('city-list').text(city.name).attr('id', city.id).text(city.name);
+                    ;
                     suggestionsDiv.append(suggestion);
+                    document.getElementById(city.id).addEventListener('click', function () {
+                        var id_city = localStorage.getItem("id_city")
+                        var arr_city;
+                        if (id_city === null) {
+                            arr_city = [{'one': city.id, 'two': null, 'three': null}]
+                            localStorage.setItem("id_city", JSON.stringify(arr_city));
+                        } else {
+                            var storedNames1 = JSON.parse(localStorage.getItem("id_city"));
+                            if (storedNames1[0]['two'] === null) {
+                                arr_city = [{'one': city.id, 'two': storedNames1[0]['one'], 'three': null}]
+                                localStorage.setItem("id_city", JSON.stringify(arr_city))
+                            } else {
+                                arr_city = [{
+                                    'one': city.id,
+                                    'two': storedNames1[0]['one'],
+                                    'three': storedNames1[0]['two']
+                                }]
+                                localStorage.setItem("id_city", JSON.stringify(arr_city))
+                            }
+                        }
+                        GetWeatherCityId()
+                        $('#Hints').html("");
+
+                    })
                 });
             })
             .catch(error => {
                 console.error('Ошибка при выполнении запроса:', error);
             });
     }
+
+    GetWeatherCityId()
+
+    function GetWeatherCityId() {
+        var city_storage = localStorage.getItem("id_city")
+        if (city_storage === null) {
+            var city_id_arr = [{'one': 615, 'two': 841, 'three': 172}]
+            localStorage.setItem("id_city", JSON.stringify(city_id_arr))
+            var city_id = city_id_arr[0]
+        } else {
+            var city_id = JSON.parse(city_storage)[0]
+
+        }
+
+        $.getJSON('/citySearch/?OneCity=' + city_id['one'] + '&CityTwo=' + city_id['two'] + '&CityThree=' + city_id['three'], function (data) {
+            for (var i = 0; i < data.length; i++) {
+                console.log(data[i])
+                var city = data[i]['name']
+                var subject = data[i]['subject']
+                var weather = data[i]['weather']
+                $('#Title-' + i).html("");
+                $('#temperature-' + i).html("");
+                $('#content-' + i).html("");
+                $('#Title-' + i).append('<h1 class="city-title">' + city + '</h1><h2 class="city-subject">' + subject + '</h2>')
+                $('#temperature-' + i).append('<h1 id="city-temperature-' + i + '" class="city-temperature">' + weather['DatetimeNow']['temperature'] + '</h1>')
+                $('#temperature-' + i).append('<div class="circle"></div>')
+                $('#container-days-' + i).html("")
+
+                for (var ij = 0; ij < weather['ArrWeather'].length; ij++) {
+                    console.log(i)
+                    $('#container-days-' + i).append('<div class="container-hour"><div id="container-' + i + '-weather-' + ij + '" class="weather-past-temp"></div><div id="container-' + i + '-time-' + ij + '" ></div></div>')
+                    $('#container-' + i + '-weather-' + ij).append('<h1 class="temp-hour">' + weather['ArrWeather'][ij]['temperature'] + '</h1><div class="circle"></div>')
+                    $('#container-' + i + '-time-' + ij).append('<h2 class="time-for-hour">' + weather['ArrWeather'][ij]['time'] + '</h2>')
+                }
+            }
+        })
+    }
+
 });
 
